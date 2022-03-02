266e7bd16df9e1722451aaa18adc31b23d0f3f6f
Sebastian Tremmel <littleperlmonk@gmail.com>
2022-03-02 11:00:56 +0100

    fix: added mock auth service to v20 da app test

--- apps/v20-dark-ages/src/app/app.component.spec.ts
+++ apps/v20-dark-ages/src/app/app.component.spec.ts
@@ -1,14 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '@auth0/auth0-angular';
 import { V20DaHomeComponent } from '@timesink/feature-v20-home';
 import { UiModule } from '@timesink/ui';
 import { AppComponent } from './app.component';
+
+export class MockAuthService {
+  public handleAuth(): void {
+    return;
+  }
+}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      providers: [{ provide: AuthService, useValue: new MockAuthService() }],
       imports: [
         BrowserAnimationsModule,
         UiModule,
